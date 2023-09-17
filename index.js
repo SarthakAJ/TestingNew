303b7842b473d13e882105841cc7de77c75d52b7
Sarthak Jaybhay <sarthakjaybhay2221@gmail.com>
2023-09-17 23:19:40 +0530

    changes made

--- index.js
+++ index.js
@@ -7,14 +7,14 @@ const passport = require('passport');
 require('./models/User.js');
 require('./services/passport.js');
 
-const port = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(keys.mongoURI).then(result => {
-    app.listen(port);
-    console.log("Server running through port : " + port);
+    app.listen(PORT);
+    console.log("Server running through port : " + PORT);
     console.log("Connected to database");
 }).catch(err => console.log(err));
-
+require('./routes/authRoutes')(app);
 app.use(cookieSession({              // app.use is a middleware method => small functions that can be used to modify incoming requests
                                      // to our app before going to route handlers
     maxAge : 30 * 24 * 60 * 60 * 1000,
@@ -24,4 +24,3 @@ app.use(cookieSession({              // app.use is a middleware method => small 
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
